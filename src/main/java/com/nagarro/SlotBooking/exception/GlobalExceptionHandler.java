d4de88140cabaadf0a19f37ceedc264a1fd791db
xduche-28 <xduche884@gmail.com>
2024-07-12 21:59:59 +0530

    second commit

--- src/main/java/com/nagarro/SlotBooking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nagarro/SlotBooking/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.nagarro.SlotBooking.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ApiException exception,
                                                                             WebRequest webRequest) {
         ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
